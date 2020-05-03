68a9c5ef7d44a0d3a27d4962c13b9e07abb9d33f
fingerskier <fingerskier@gmail.com>
2020-05-03 15:12:02 -0500

    initial content

--- assets/js/class/gameManager/utils.js
+++
@@ -1,8 +0,0 @@
-export const SpawnerType = {
-  CHEST: 'CHEST',
-  MONSTER: 'MONSTER',
-}
-
-export function randomNumber(min,max) {
-  return Math.floor(Math.random() * max) + min
-}
---
+++ client/src/index.js
@@ -0,0 +1,31 @@
+import * as Phaser from 'phaser';
+import scenes from './scenes/scenes';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: scenes,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+      gravity: {
+        y: 0,
+      },
+    },
+  },
+  pixelArt: true,
+  roundPixels: true,
+};
+
+class Game extends Phaser.Game {
+  constructor() {
+    super(config);
+    this.scene.start('Boot');
+  }
+}
+
+window.onload = () => {
+  window.game = new Game();
+};
